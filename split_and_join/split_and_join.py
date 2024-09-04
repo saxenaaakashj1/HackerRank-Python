c309d575b2edea6982b96bd0881ca0bf673adbde
saxenaaakashj1 <saxenaaakashj1@gmail.com>
2024-09-04 16:22:24 +0530

    🚀 First Commit 🚀

---
+++ split_and_join/split_and_join.py
@@ -0,0 +1,12 @@
+def main():
+    line = input()
+    print(split_and_join(line))
+
+def split_and_join(line):
+    # Split the input string by spaces and join the resulting list with hyphens
+    # line.split(" ") splits the string into a list of substrings
+    # "-".join() joins the substrings with hyphens
+    return "-".join(line.split(" "))
+
+if __name__ == "__main__":
+    main()
