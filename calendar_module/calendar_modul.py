e139dabe26dbab94c296859e93fad64a8fce7c1d
saxenaaakashj1 <saxenaaakashj1@gmail.com>
2025-06-18 21:06:38 +0530

    🚀first commit🚀

--- calendar_module/calendar_modul.py
+++ calendar_module/calendar_modul.py
@@ -1,14 +1,14 @@
 import calendar  # Import the built-in calendar module
 
-# Take input from the user in the format MM DD YYYY, split it, 
+# Take input from the user in the format MM DD YYYY, split it,
 # and convert each to an integer
 month, date, year = map(int, input().split())
 
-# Get the index of the weekday (0 = Monday, 6 = Sunday) using 
+# Get the index of the weekday (0 = Monday, 6 = Sunday) using
 # calendar.weekday
 weekday_index = calendar.weekday(year, month, date)
 
-# Get the name of the day from calendar.day_name using the index, 
+# Get the name of the day from calendar.day_name using the index,
 # then convert it to uppercase
 day_name = calendar.day_name[weekday_index].upper()
 
